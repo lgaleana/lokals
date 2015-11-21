d7090c8c9681d79777c25345c09dcbfdb8eeb404
Samuel Heaney <sheaney@gmail.com>
2015-11-21 16:45:23 -0600

    Mini chat implementation with SSE

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,45 @@
 package controllers
 
-import play.api._
+import play.api.libs.EventSource
+import play.api.libs.iteratee.{Enumeratee, Enumerator, Concurrent}
+import play.api.libs.json.{Json, JsValue}
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val (chatOut, chatChannel) = Concurrent.broadcast[JsValue]
+
+  // Central hub for distributing chat messages
+  def index = Action { implicit req =>
+    Ok(views.html.index(routes.Application.chatFeed, routes.Application.postMessage))
+  }
+
+  // Detects disconnect SSE stream
+  def connDeathWatch(addr: String): Enumeratee[JsValue, JsValue] =
+    Enumeratee.onIterateeDone{ () => println(addr + " - SSE disconnected") }
+
+  def welcome = Enumerator.apply[JsValue](
+    Json.obj(
+      "user"    -> "Local",
+      "message" -> "Welcome! Write a message and hit ENTER!"
+    )
+  )
+
+  // Serves chat msgs to clients
+  def chatFeed = Action { req =>
+    println("Someone connected" + req.remoteAddress)
+    Ok.chunked(
+      welcome >>> chatOut
+        &> connDeathWatch(req.remoteAddress)
+        &> EventSource()
+    ).as("text/event-stream")
+  }
+
+  // Posts chat msgs
+  def postMessage = Action(parse.json) { req =>
+    chatChannel.push(req.body)
+    Ok
   }
 
 }
